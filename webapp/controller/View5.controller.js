d596fdc1d4478a641251ae6eca13989c2c63431e
Nikita Sawant <nikitasawant243@gmail.com>
2026-04-07 04:36:29 +0000

    Added View5 controller and updated manifest

---
+++ webapp/controller/View5.controller.js
@@ -0,0 +1,42 @@
+
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/ValueState"
+    
+], function (Controller, JSONModel, ValueState) {
+    "use strict";
+
+    return Controller.extend("demo.practice.practice.controller.View5", {
+
+        onInit: function () {
+            var oModel = new JSONModel({
+                email: "",
+                length:0
+            });
+            this.getView().setModel(oModel);
+
+        },
+
+        onLiveChange: function (oEvent){
+            var sValue = (oEvent.getParameter("value") || "").trim();
+            var oInput = this.byId("emailInput");
+            var bValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(sValue);
+
+    
+
+            //validation
+            if(!sValue) {
+                oInput.setValueState(ValueState.None);
+                oInput.setValueStateText("");
+            } else if (bValid){
+                oInput.setValueState(ValueState.Success);
+                oInput.setValueStateText("Valid email");
+            } 
+            else {
+                oInput.setValueState(ValueState.Error);
+                oInput.setValueStateText("Enter email like abc@xyz.com");
+            }
+        }
+    });
+});
